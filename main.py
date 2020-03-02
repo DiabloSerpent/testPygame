03e009e7a86c509c3a03f50d759082d8ce6c04ec
DiabloSerpent <M1ddleS3c>
2020-03-02 13:10:09 -0600

    Added:  - FPS  - A second message  - Ability to quit the program from the window    (the x button)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 bounds = pygame.Rect(0, 0, 800, 600)
+ticker = pygame.time.Clock()
+running = True
 
 screen.fill((255, 0, 0))
 
@@ -15,11 +17,24 @@ esc_rect.center = bounds.center
 esc_image = esc.render(esc_msg, 0, (0, 255, 0), (255, 0, 0))
 
 screen.blit(esc_image, esc_rect)
+
+srsly = pygame.font.Font(None, 60)
+srsly_msg = "SRSLY"
+srsly_rect = pygame.Rect((0, 0), srsly.size(srsly_msg))
+srsly_rect.center = bounds.center
+srsly_rect.move_ip(0, 100)
+srsly_image = esc.render(srsly_msg, 0, (0, 0, 255), (255, 0, 0))
+
+screen.blit(srsly_image, srsly_rect)
+
 pygame.display.flip()
 
 
-while True:
-    pygame.event.pump()
-    keys = pygame.key.get_pressed()
-    if keys[pgv.K_ESCAPE]:
-        break
+while running:
+    for ev in pygame.event.get():
+        if ev.type == pgv.KEYDOWN:
+            if ev.key == pgv.K_ESCAPE:
+                running = False
+        elif ev.type == pgv.QUIT:
+            running = False
+    ticker.tick(60)
